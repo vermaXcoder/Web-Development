4448eb1e1a0820f95b7031d9fe4964fd3af88824
Rishabh Raj <irishabhrajverma@gmail.com>
2025-12-30 21:44:22 +0530

    convresion of data types

--- WEBSITE/JAVASCRIPT/01_basics/conversionOperation.js
+++ WEBSITE/JAVASCRIPT/01_basics/conversionOperation.js
@@ -1,10 +1,33 @@
-let scope = "33abc"
+let score ="dfhe"
 
-console.log(typeof score);
-console.log(typeof (score));
+
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 
+// "33" => 33
+// "33abc"=>NaN
+// true => 1; false =>0
+
+let isLoggedIn = "hdi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1=> true ; 0=>false
+// "" => false
+// "hitesh"=> true
+
+
+
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof  stringNumber);
