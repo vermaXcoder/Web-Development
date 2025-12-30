e4229a6ebdb77d6a2b9c077bfac64d54de87f79b
Rishabh Raj <irishabhrajverma@gmail.com>
2025-12-30 22:26:18 +0530

    conversion

--- WEBSITE/JAVASCRIPT/01_basics/conversionOperation.js
+++ WEBSITE/JAVASCRIPT/01_basics/conversionOperation.js
@@ -1,33 +1,56 @@
-let score ="dfhe"
+// let score ="dfhe"
 
 
-// console.log(typeof score);
-// console.log(typeof (score));
+// // console.log(typeof score);
+// // console.log(typeof (score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log( valueInNumber);
-
-
-// "33" => 33
-// "33abc"=>NaN
-// true => 1; false =>0
-
-let isLoggedIn = "hdi"
-
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log( valueInNumber);
 
 
-// 1=> true ; 0=>false
-// "" => false
-// "hitesh"=> true
+// // "33" => 33
+// // "33abc"=>NaN
+// // true => 1; false =>0
+
+// let isLoggedIn = "hdi"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
+
+
+// // 1=> true ; 0=>false
+// // "" => false
+// // "hitesh"=> true
 
 
 
 
-let someNumber =33
+// let someNumber =33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof  stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof  stringNumber);
+
+// *************************Operations**********
+
+let value =3
+let negvalue=-value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(negvalue);
+// console.log(2-2);
+// console.log(5*3);
+// console.log(3**3);
+// console.log(3/4);
+// console.log(3%4);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2+ "2");
+
+
+
+
