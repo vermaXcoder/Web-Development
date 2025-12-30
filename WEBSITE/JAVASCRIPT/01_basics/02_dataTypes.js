f61dcc39141f5e7262ef580b9655919f887d122e
Rishabh Raj <irishabhrajverma@gmail.com>
2025-12-30 21:31:05 +0530

    variables

---
+++ WEBSITE/JAVASCRIPT/01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+" use strict ";// treat all JS as newer version
+
+//alert (3+4)// we are using node.js not browser
+
+ console.log(3
+    +3);  // code readability sdould be high 
+    
+
+
+console.log("Rishabh Raj")
+ 
+let name = "name"
+let age = 12
+let isLoggedIn= false
+let state ;
+
+
+
+//  number => 2 to power 53
+//bigint
+// string=> ""
+ // boolean => true/false
+ //null=> standalone value
+ // undefined => 
+    
+ // symbol => unique
+
+
+ //object
+
+ console.log(typeof "name");
+ console.table([typeof "name",typeof age , typeof isLoggedIn ,typeof state ,typeof null])
+ 
---
+++ WEBSITE/JAVASCRIPT/01_basics/conversionOperation.js
@@ -0,0 +1,10 @@
+let scope = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
